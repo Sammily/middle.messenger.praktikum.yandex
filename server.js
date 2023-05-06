0df01ad63cc9b102557d0ee87003381bb306c7ed
Sammily <sammily03@yandex.ru>
2023-05-06 21:59:43 +0300

    fix: fix server config

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const PORT = 3000;
 
 const app = express();
  
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/dist"));
  
 app.listen(PORT, () => {
   console.log(`Мой текст в логе после запуска ${PORT}!`);
