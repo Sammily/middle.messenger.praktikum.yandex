9385207b476ed3b0dbe706b0fe37a877896dfea4
Sammily <sammily03@yandex.ru>
2023-07-10 11:57:51 +0300

    fix: fixed error on page reload

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const fallback = require('express-history-api-fallback');
 const PORT = 3000;
 
 const app = express();
- 
-app.use(express.static(__dirname + "/dist"));
- 
+const root = __dirname + "/dist";
+app.use(express.static(root));
+app.use(fallback("index.html", { root }));
+
 app.listen(PORT, () => {
   console.log(`Мой текст в логе после запуска ${PORT}!`);
 });
